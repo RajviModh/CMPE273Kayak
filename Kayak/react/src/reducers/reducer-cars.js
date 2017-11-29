749a8d7557b5cc98f243ca54e0d45777697db255
rohanath <rohan.athavale@sjsu.edu>
2017-11-29 00:53:59 -0800

    Commiting with booking insertion

--- Kayak/react/src/reducers/reducer-cars.js
+++ Kayak/react/src/reducers/reducer-cars.js
@@ -1,13 +1,39 @@
 const reducerCars = (state = {
-    cars: []
+    cars: [],
+    selectedcars : {},
+    cardetails : {},
+    carbookingdetails:{}
 }, action) => {
     switch (action.type) {
+
         case "STORECARS":
             state = {
                 ...state,
                 cars: action.payload.data
             };
             break;
+
+        case "STORESELECTEDCARS":
+            state = {
+                ...state,
+                selectedcars: action.payload.data
+        };
+        break;
+
+        case "STOREUSERDETAILS":
+            state = {
+                ...state,
+                cardetails: action.payload.data
+            };
+            break;
+
+        case "STORECARBOOKINGREQUEST"   :
+            state = {
+                ...state,
+                carbookingdetails: action.payload.data
+            };
+            break;
+
 
         default:
             break;
