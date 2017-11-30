b17cd9455df2997ef0e93a5efa61d3936ef73d9c
Anurag Panchal <anurag3110@outlook.com>
2017-11-29 16:06:45 -0800

    Hotel Booking Complete

--- Kayak/react/src/api/API.js
+++ Kayak/react/src/api/API.js
@@ -126,6 +126,356 @@ export const adminSearchHotels = (payload) => {
         });
 };
 
+export const adminSearchFlights = (payload) => {
+    return fetch(`${api}/adminSearchFlights`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminSearchCars = (payload) => {
+    return fetch(`${api}/adminSearchCars`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+
+export const adminSearchBills = (payload) => {
+    return fetch(`${api}/adminSearchBils`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+// -----------------------------------------------Admin View Operations-----------------------------------------------
+
+export const adminViewHotels = (payload) => {
+    return fetch(`${api}/adminViewHotels`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminViewFlights = (payload) => {
+    return fetch(`${api}/adminViewFlights`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminViewCars = (payload) => {
+    return fetch(`${api}/adminViewCars`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminViewUsers = (payload) => {
+    return fetch(`${api}/adminViewUsers`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+// -----------------------------------------------Admin Update Operations-----------------------------------------------
+
+export const adminUpdateHotels = (payload) => {
+    return fetch(`${api}/adminUpdateHotels`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminUpdateFlights = (payload) => {
+    return fetch(`${api}/adminUpdateFlights`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            alert(" FLIGHT in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminUpdateCars = (payload) => {
+    return fetch(`${api}/adminUpdateCars`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminUpdateUsers = (payload) => {
+    return fetch(`${api}/adminUpdateUsers`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+// -----------------------------------------------Admin Delete Operations-----------------------------------------------
+
+
+export const adminDeleteHotels = (payload) => {
+    return fetch(`${api}/adminDeleteHotels`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminDeleteFlights = (payload) => {
+    return fetch(`${api}/adminDeleteFlights`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminDeleteCars = (payload) => {
+    return fetch(`${api}/adminDeleteCars`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+
+// -----------------------------------------------Admin Dashboard Operations-----------------------------------------------
+
+export const adminViewCityByRevenue = (payload) => {
+    return fetch(`${api}/adminViewCityByRevenue`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminViewHotelsChart = (payload) => {
+    return fetch(`${api}/adminViewHotelsChart`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
+
+export const adminViewCarsChart = (payload) => {
+    return fetch(`${api}/adminViewCarsChart`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+    ).then(res => res.json())
+        .then(res => {
+            //alert("in api response : "+JSON.stringify(res));
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+};
 //----------------------------------------------------Hotel API calls---------------------------------------------------
 
 export const searchHotels = (payload) => {
