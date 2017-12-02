7ea421f96ec97fd33749f23886956aaa01f36c1d
rohanath <rohan.athavale@sjsu.edu>
2017-12-02 03:04:25 -0800

    Commiting with Redis

--- Kayak/kafka-back-end/services/hotel/hotelSearch.js
+++ Kayak/kafka-back-end/services/hotel/hotelSearch.js
@@ -1,4 +1,5 @@
 const mysql = require('../mysql');
+const redis = require('redis');
 
 function handleRequest(data, callback) {
 
@@ -11,8 +12,9 @@ function handleRequest(data, callback) {
     };*/
 
     let hotelAvailabilityProcedureCallSQL = "CALL kayak.hotel_availability('" + data.city + "', '" + data.fromDate + "', '" + data.toDate + "', " + data.requiredNoOfRooms + ");";
+    let id = hotelAvailabilityProcedureCallSQL;
 
-    mysql.fetchData((error, rows) => {
+    /*mysql.fetchData((error, rows) => {
         if (error) {
             response.code = 500;
             response.message = "Server Error";
@@ -29,9 +31,57 @@ function handleRequest(data, callback) {
         }
 
 
-    }, hotelAvailabilityProcedureCallSQL);
+    }, hotelAvailabilityProcedureCallSQL);*/
 
+    client.get(id, function (err, obj) {
+
+        console.time("Query_time");
+
+        if (err) {
+            console.log(err);
+        }
+
+        if (!obj) {
+
+            mysql.fetchData((error, rows) => {
+                if (error) {
+                    response.code = 500;
+                    response.message = "Server Error";
+                    callback(error, response);
+                } else {
+                    console.time("Query_time");
+                    let availableHotels = JSON.parse(rows[0][0].availableHotels);
+                    console.log(availableHotels);
+                    response.code = 200;
+                    response.message = "Search Successful";
+                    response.payload = availableHotels;
+                    client.set(id, JSON.stringify(response.payload), function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                    client.expire(id, 30);
+                    callback(null, response);
+                }
+
+            }, hotelAvailabilityProcedureCallSQL);
+
+        }
+
+        else {
+            obj = JSON.parse(obj);
+            //console.log(obj);
+            console.timeEnd("Query_time");
+            console.log("From redis");
+
+            //let availableHotels = JSON.parse(obj.availableHotels);
+            console.log(availableHotels);
+            response.code = 200;
+            response.message = "Search Successful";
+            response.payload = obj;
+            callback(null, response);
+        }
+    })
 }
-
 
 exports.handleRequest = handleRequest;
